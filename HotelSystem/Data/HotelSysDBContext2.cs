02d2f6423f74cc7b976ebc6acf35eacba6f3ed28
Violet <violet87120win@gmail.com>
2025-07-18 16:31:04 +0800

    7/18 課程

---
+++ HotelSystem/Data/HotelSysDBContext2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using HotelSystem.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelSystem.Data;
+
+public partial class HotelSysDBContext2 : HotelSysDBContext
+{
+    public HotelSysDBContext2(DbContextOptions<HotelSysDBContext> options)
+        : base(options)
+    {
+    }
+
+    public int GetRoomServiceCount()
+    {
+        return RoomService.CountAsync().Result;
+    }
+}
